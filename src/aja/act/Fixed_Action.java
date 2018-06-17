a3ff298165115ae15c635e6d6dc7090d5a703156
t_ues <t_ues@192.168.3.25>
2018-06-17 16:31:44 +0900

    6/17 上里

--- src/aja/act/Fixed_Action.java
+++ src/aja/act/Fixed_Action.java
@@ -6,6 +6,15 @@ public class Fixed_Action extends Action {
 
 	@Override
 	public String execute(HttpServletRequest request) throws Exception {
+
+		//addDaoのaddOrder()を利用して、Buy_Detail,Item_Reserverにレコードを追加
+		//addDao実装後importしたあと、コメントアウト状態を解除すること
+
+		/*
+		AddDao addDao = new AddDao();
+		addDao.addOrder();
+		*/
+
 		return "/fixed.jsp";
 	}
 
--- src/aja/act/Item_Detail_List_Action.java
+++ src/aja/act/Item_Detail_List_Action.java
@@ -6,6 +6,19 @@ public class Item_Detail_List_Action extends Action {
 
 	@Override
 	public String execute(HttpServletRequest request) throws Exception {
+		//リクエスト処理
+		request.setCharacterEncoding("UTF-8");
+		String itemName = request.getParameter("itemName");
+
+		//ListDaoのsitemDetailList()を利用してグッズの詳細情報を取得
+		//ListDao実装後importしたあと、コメントアウト状態を解除すること
+
+		/*
+		ListDao listDao = new ListDao();
+		ItemBean detail = listDao.itemDetailList(itemName);
+		request.setAttribute("detail",detail);
+		*/
+
 		return "/itemDetailList.jsp";
 	}
 
