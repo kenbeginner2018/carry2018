23e8371068fa891cc946169ac87ceb0d5974dfaf
t_ues <t_ues@192.168.3.25>
2018-06-17 15:52:40 +0900

    6/17 上里

--- src/aja/act/Select_Show__Action.java
+++ src/aja/act/Select_Show__Action.java
@@ -6,7 +6,15 @@ public class Select_Show__Action extends Action {
 
 	@Override
 	public String execute(HttpServletRequest request) throws Exception {
-		
+
+		//ListDaoのshowList()を利用して表示させる公演を取得
+		//ListDao実装後importしたあと、コメントアウト状態を解除すること
+
+		/*
+		ListDao listDao = new ListDao();
+		ArrayList<ShowBean> show = listDao.showList();
+		request.setAttribute("show",show);
+		*/
 		return "/select.jsp";
 	}
 
