5a2dafe2b291e0d6092d7a55fe4f3105a3325062
t_ues <t_ues@192.168.3.25>
2018-06-17 17:50:07 +0900

    6/17 上里

--- src/aja/act/Reservation_List_Action.java
+++ src/aja/act/Reservation_List_Action.java
@@ -6,7 +6,17 @@ public class Reservation_List_Action extends Action {
 
 	@Override
 	public String execute(HttpServletRequest request) throws Exception {
+
+		//受け取ったrequest情報をキーとしてListDaoのreservationList()メソッドを用いて
+		//条件に当てはまる予約情報を取得する（設計書から微妙に仕様が変更。要確認。）
+		//ListDaoが実装され次第、コメントアウト状態を解除すること
+
+		/*
+		ListDao listDao = new ListDao();
+		ArrayList<Reservation_ListBean> rList = listDao.reservationList(request);
+		request.setAttribute("rList",rList);
+		*/
+
 		return "/reservationList.jsp";
 	}
-
 }
