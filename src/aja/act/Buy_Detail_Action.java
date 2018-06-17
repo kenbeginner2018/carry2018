ce6cebcaf88b91873fe169bdc03ea734ef540955
t_ues <t_ues@192.168.3.25>
2018-06-17 17:41:42 +0900

    6/17 上里

--- src/aja/act/Buy_Detail_Action.java
+++ src/aja/act/Buy_Detail_Action.java
@@ -6,8 +6,18 @@ public class Buy_Detail_Action extends Action {
 
 	@Override
 	public String execute(HttpServletRequest request) throws Exception {
-		
-		
+
+		int reservNo = Integer.parseInt(request.getParameter("reservNo"));
+
+		//reservNoをキーとしてListDaoのorderList()メソッドにより対応した顧客の購入情報を取得する
+		//ListDao実装後、コメントアウト状態を外すこと
+
+		/*
+		ListDao listDao = new ListDao();
+		ArrayList<OrderBean> oList = listDao.orderList(request);
+		request.setAttribute("oList", oList);
+		*/
+
 		return "/buyDetail.jsp";
 	}
 }
