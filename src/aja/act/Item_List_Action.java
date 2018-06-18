6bfc4fe3b40c0e202609e671369919deb2bf39ef
ken-newc <ken-newc@192.168.11.43>
2018-06-18 13:51:09 +0900

    6/18 上里

--- src/aja/act/Item_List_Action.java
+++ src/aja/act/Item_List_Action.java
@@ -88,7 +88,6 @@ public class Item_List_Action extends Action {
 		return "/itemList.jsp";
 	}
 
-
 	//電話番号の正規表現チェック(ハイフンが正しい位置に入力されているか否か)
 	private boolean checkTelNo(String telNo) {
 		String regex = "^[0-9]+-[0-9]+-[0-9]+$";
