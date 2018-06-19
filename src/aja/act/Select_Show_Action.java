00015c3a84c88f85f3b48f62229df415b2f4abf0
ken-newc <ken-newc@192.168.11.43>
2018-06-19 11:15:53 +0900

    6/19 上里

--- src/aja/act/Select_Show_Action.java
+++ src/aja/act/Select_Show_Action.java
@@ -1,10 +1,7 @@
 package aja.act;
 
-import java.util.ArrayList;
-
 import javax.servlet.http.HttpServletRequest;
 
-import aja.bean.ShowBean;
 import aja.dao.ListDAO;
 
 public class Select_Show_Action extends Action {
@@ -16,8 +13,8 @@ public class Select_Show_Action extends Action {
 		//ListDao実装後importしたあと、コメントアウト状態を解除すること
 
 		ListDAO listDao = new ListDAO();
-		ArrayList<ShowBean> show = listDao.show_List();
-		request.setAttribute("show",show);
+//		ArrayList<ShowBean> show = listDao.show_List();
+//		request.setAttribute("show",show);
 
 		return "/select.jsp";
 	}
