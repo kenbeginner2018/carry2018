686eb6380ea33d4f1d152583238300293964658d
ken-newc <ken-newc@192.168.11.4>
2018-06-20 14:45:59 +0900

    6/20 DB徳山

--- db/insert_showd.sql
+++ db/insert_showd.sql
@@ -1,5 +1,7 @@
-insert into showd(
+insert into t_order.showd(
 showId,showName,showStartDay,showEndDay,showFlag)
 values
 ('1','äMê˘ñÂ','20180601','20180701','0'),
-('2','MESSIAH','20180702','20180802','1');
+('2','MESSIAH','20180702','20180802','1')
+('3','ANOTHER WORLD','20180401','20180501','0'),
+('4','Killer Rauge','20180901','20181001','0');
--- db/showd.sql
+++ db/showd.sql
@@ -1,7 +1,9 @@
-create table showd(
+create table t_order.showd(
 showId integer not null auto_increment,
 showName varchar(40) not null,
 showStartDay date not null,
 showEndDay date not null,
 showFlag integer not null,
+showPast date not null,
+showFuture date not null,
  primary key(showId));
