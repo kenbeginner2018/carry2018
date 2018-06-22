7a2b7abc61907fa70657cc6c8b86b30f0c82b281
ken-newc <ken-newc@192.168.11.24>
2018-06-22 11:42:39 +0900

    片渕　コメント追加

--- src/aja/act/Content_List_Action.java
+++ src/aja/act/Content_List_Action.java
@@ -17,7 +17,6 @@ public class Content_List_Action extends Action {
 		ArrayList<OrderBean> cart = (ArrayList<OrderBean>) session.getAttribute("cart");
 
 	//session.cart実装をスムーズに行うため、いったんクレカの処理を止めています。
-	//実装終了後、コメントアウトを解除してください
 
 		//16桁のcreditNoを生成する。
 
@@ -26,65 +25,47 @@ public class Content_List_Action extends Action {
 		String name = request.getParameter("name");
 		String securityCode = request.getParameter("securityCode");
 
-		//creditNo,securityCodeの空入力チェック および 正規表現チェック
-
+		//creditNoのから入力、不正アクセスチェック
 		if(request.getParameter("creditNo1-4") == null || request.getParameter("creditNo1-4").equals("") ||
 				request.getParameter("creditNo5-8") == null || request.getParameter("creditNo5-8").equals("")||
 				request.getParameter("creditNo9-12") == null || request.getParameter("creditNo9-12").equals("")||
 				request.getParameter("creditNo13-16") == null || request.getParameter("creditNo13-16").equals("")) {
 			request.setAttribute("errorMessage", "クレジット番号が入力されていません");
 			return "/credit.jsp";
-		}else if(name == null || name.equals("")){
+		}
+		//名前のから入力、不正アクセス
+		else if(name == null || name.equals("")){
 			request.setAttribute("errorMessage", "名前が入力されていません");
 			return "/credit.jsp";
-		}else if(securityCode == null || securityCode.equals("")) {
+		}
+		//securityCodeの空入力、不正アクセス
+		else if(securityCode == null || securityCode.equals("")) {
 			request.setAttribute("errorMessage", "セキュリティコードが入力されていません");
 			return "/credit.jsp";
-		}else if(!checkCreditNo(creditNo)) {
+		}
+		//creditNoの正規チェック
+		else if(!checkCreditNo(creditNo)) {
 			request.setAttribute("errorMessage", "クレジット番号を正しく入力してください");
 			return "/credit.jsp";
-		}else if(!checkName(name)) {
+		}
+		//名前の正規チェック
+		else if(!checkName(name)) {
 			request.setAttribute("errorMessage", "名前は以下の形式で入力してください<br />(例)TANAKA TARO");
 			return "/credit.jsp";
-		}else if(!checkSecurityCode(securityCode)) {
+		}
+		//securityCodeの正規チェック
+		else if(!checkSecurityCode(securityCode)) {
 			request.setAttribute("errorMessage", "セキュリティコードを正しく入力してください");
 			return "/credit.jsp";
 		}
 
-	/*
-		//以下、session.cartをでっちあげます。
-		//テスト終了後、削除してください
-		HttpSession session = request.getSession(true);
-		ArrayList<OrderBean> cart = new ArrayList<OrderBean>();
-
-		OrderBean addItem = new OrderBean();
-		addItem.setReservNo(1111111);
-		addItem.setItemName("ブレスレット");
-		addItem.setItemPrice(3300);
-		addItem.setItemCount(1);
-		addItem.setSubTotal(3300);
-		cart.add(addItem);
-		addItem = new OrderBean();
-		addItem.setReservNo(1111111);
-		addItem.setItemName("オードトワレ");
-		addItem.setItemPrice(4600);
-		addItem.setItemCount(2);
-		addItem.setSubTotal(9200);
-		cart.add(addItem);
-
-		LoginBean login = new LoginBean();
-		login.setReservNo(1111111);
-		login.setTelNo("test");
-		session.setAttribute("login",login);
-
-		session.setAttribute("cart",cart);
-
-	*/
-
 		//お買い上げ総額計算用
 		cart = (ArrayList<OrderBean>) session.getAttribute("cart");
 		int totalPrice = 0;
+
+		//カートの中の商品種類の回数繰り返す
 		for(OrderBean oBean : cart) {
+			//小計を足していき合計金額を求める
 			totalPrice += oBean.getSubTotal();
 		}
 
