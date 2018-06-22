6e71a9784991042b39f6d73f60ce97a0c1050959
ken-newc <ken-newc@192.168.11.24>
2018-06-22 11:57:51 +0900

    片渕 コメント追加

--- src/aja/act/Reservation_List_Action.java
+++ src/aja/act/Reservation_List_Action.java
@@ -29,8 +29,6 @@ public class Reservation_List_Action extends Action {
 
 		//受け取ったrequest情報をキーとしてListDaoのreservationList()メソッドを用いて
 		//条件に当てはまる予約情報を取得する
-
-
 		//初遷移の場合(showYear,showMonth,showDayがnullの場合)、処理を飛ばす
 		if(request.getParameter("showYear") != null && request.getParameter("showMonth") != null &&
 				request.getParameter("showDay") != null) {
@@ -41,7 +39,9 @@ public class Reservation_List_Action extends Action {
 				String errorMessage="日時を指定してください";
 				request.setAttribute("errorMessage",errorMessage);
 
-			}else {
+			}
+			//日時が入力されていた場合以下の処理を行う
+			else {
 				ListDAO listDao = new ListDAO();
 				ArrayList<Reservation_ListBean> rList = listDao.reservation_List(request);
 				request.setAttribute("rList",rList);
