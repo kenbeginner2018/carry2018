f3930f9439c9307b0383402ff7460af297ac7ffd
ken-newc <ken-newc@192.168.11.24>
2018-06-22 09:55:38 +0900

    片渕　LoginUser

--- src/aja/act/Login_User_Action.java
+++ src/aja/act/Login_User_Action.java
@@ -21,7 +21,15 @@ public class Login_User_Action extends Action {
 		//loginがsessionに存在すれば、loginページをスキップしてitemList.jspへ遷移
 
 		if(login != null) {
+
+			//公演日時と選択された公演の日付をチェック
+
+			//公演内のチケットだったら
 			return "/itemList.jsp";
+
+			//そうでなかったら
+				//エラーメッセージに”異なるチケットでログイン中でした”
+				return "/loginUser.jsp";
 		}else {
 			return "/loginUser.jsp";
 		}
